56011d5f60a33aa9dbb9fffae22c724031389972
Muhammad Arslan <muhammadarslanishtiaq@gmail.com>
2022-07-29 11:06:35 +0500

    bilingual feature  added

--- src/components/footer/FooterPrimary.jsx
+++ src/components/footer/FooterPrimary.jsx
@@ -1,20 +1,21 @@
+import { useTranslation } from "react-i18next";
+
 export default function FooterPrimary() {
+  const { t } = useTranslation();
   return (
     <footer
-      className='bg-primary text-center lg:text-left'
-      aria-labelledby='footer-heading'
+      className="bg-primary text-center lg:text-left"
+      aria-labelledby="footer-heading"
     >
-      <h2 id='footer-heading' className='sr-only'>
+      <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
-      <div className='lg:max-w-7xl px-6 mx-auto '>
-        <div className='py-6 flex flex-wrap gap-4 md:items-center'>
-          <p className='text-lg text-white mx-auto lg:ml-0 lg:mr-auto '>
-            私隱政策 | 聲明免責聲明及使用條款
+      <div className="lg:max-w-7xl px-6 mx-auto ">
+        <div className="py-6 flex flex-wrap gap-4 md:items-center">
+          <p className="text-lg text-white mx-auto lg:ml-0 lg:mr-auto ">
+            {t("Footer.1")}
           </p>
-          <p className='text-lg text-white'>
-            © 2022 版權所有。香港中華煤氣有限公司
-          </p>
+          <p className="text-lg text-white">{t("Footer.2")}</p>
         </div>
       </div>
     </footer>
